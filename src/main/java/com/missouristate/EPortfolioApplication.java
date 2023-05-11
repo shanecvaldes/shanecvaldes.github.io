bea4d29f1458b5ed5e41e826faa1130bcf6abb8b
Tonnid10 <82744537+Tonnid10@users.noreply.github.com>
2023-05-11 14:59:16 -0500

    Source

---
+++ src/main/java/com/missouristate/EPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.missouristate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/missouristate/EPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.missouristate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
